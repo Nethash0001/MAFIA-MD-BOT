d4775ace3ebe5634907400c3bc874af6fda2bbf1
Error#0001 <89148628+Nethash0001@users.noreply.github.com>
2025-01-21 02:31:19 +0400

    Update config.js
    Signed-off-by: Error#0001 <89148628+Nethash0001@users.noreply.github.com>

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "qvBwlKbS#pLg2P1i8_j3btJMrIRqacGfOASLxiwkMbH8fZMRbN_M",
     CAPTION: process.env.CAPTION || "POWERED BY MAFIA-ADEEL🌹",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
